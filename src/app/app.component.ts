49057468833293c44d25204d769163a38b83b6ac
Harshit Chouhan <harshit.chouhan@asmadiya.com>
2023-03-04 15:49:59 +0530

    UI created and made responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'currency-conversion-fe';
+
+  form = new FormGroup({
+    amount: new FormControl(1, [Validators.required, Validators.max(999999999)]),
+    from: new FormControl('USD', [Validators.required, Validators.maxLength(3)]),
+    to: new FormControl('INR', [Validators.required, Validators.maxLength(3)]),
+    convertedValue: new FormControl(81),
+  });
 }
